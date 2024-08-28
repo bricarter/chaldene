b43827dfc8b0f00b7141b9fc405898f5642e54b6
bricarter <101752437+bricarter@users.noreply.github.com>
2024-08-28 10:07:32 -0500

    feat: configure logger

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,13 @@
+import os
+import logging
+
 from flask import Flask
 
 
 def create_app():
+    os.makedirs("logs", exist_ok=True)
+    logging.basicConfig(filename="logs/app_logs.log", level="INFO")
+
     app = Flask(__name__)
 
     @app.route("/")
