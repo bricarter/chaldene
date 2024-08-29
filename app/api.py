7532d4f970df5249d37b6ff23bc09035ef8ea30f
bricarter <101752437+bricarter@users.noreply.github.com>
2024-08-28 19:19:43 -0500

    feat: endpoint to get an individual resource request

--- app/api.py
+++ app/api.py
@@ -20,3 +20,17 @@ def get_resource_requests():
         requests.append(row.__dict__)
 
     return requests
+
+
+@app.route('/api/requests/<int:id>', methods=['GET'])
+def get_individual_request(id:int):
+    connect_db()
+    requested = g.db.get(Resource, id)
+    g.db.close()
+
+    if requested is None:
+        return "that resource request is not available at this time."
+    
+    del requested.__dict__['_sa_instance_state']
+
+    return [requested.__dict__]
--- tests/test_api.py
+++ tests/test_api.py
@@ -9,7 +9,14 @@ class TestAPI(BaseTestCase):
         response = self.client.get("/api/requests")
         self.assertEqual(response.status_code, 200)
 
-
+    def test_get_individual_request(self):
+        response = self.client.get(f"/api/requests/{2}")
+        self.assertEqual(response.status_code, 200)
+        
+        # invalid id
+        response = self.client.get(f"/api/requests/{9}")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.get_data(as_text=True), "that resource request is not available at this time.")
 
 
 if __name__ == "__main__":
